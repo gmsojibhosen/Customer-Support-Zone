86d9b63fce717ea0aa7ce163d0480a882c573c9f
gmsojibhosen <gmsojibhosen@gmail.com>
2025-12-02 22:03:12 +0600

    cart btn functionality

--- src/component/CustomerTickets/CustomerTickets.jsx
+++ src/component/CustomerTickets/CustomerTickets.jsx
@@ -1,13 +1,28 @@
 import React, { use } from 'react';
 import Tickets from '../Tickets/Tickets';
+import Task from '../Task/Task';
 
 const CustomerTickets = ({customerTickets}) => {
     const tickets= use(customerTickets)
     return (
-        <div>
+        <div className='max-w-[1400px] mx-auto mt-20 grid grid-cols-8 gap-8'>
+            {/* tickets card */}
+           <div className=' col-span-6'>
+           <h2 className='font-semibold text-2xl text-[#34485A] mb-4'>Customer Tickets</h2>
+
+           <div className="grid grid-cols-2 gap-6">
             {
-                tickets.map(ticket => <Tickets ticket = {ticket}></Tickets>)
+            tickets.map(ticket => <div key={ticket.id}><Tickets ticket={ticket}> </Tickets></div>)
             }
+</div>
+            </div>
+            {/* task card */}
+           <div className='col-span-2'>
+           <h2 className='font-semibold text-2xl text-[#34485A] mb-4'>Task Status</h2>
+           <Task></Task>
+           </div>
+           
+            
         </div>
     );
 };
--- src/component/Tickets/Tickets.jsx
+++ src/component/Tickets/Tickets.jsx
@@ -1,11 +1,50 @@
 import React from 'react';
-
+import clander from '../../assets/ri_calendar-line.png'
 const Tickets = ({ticket}) => {
-    console.log(ticket)
+    // cart btn functionality
+let cartBtnText = {};   
+let cartBtnBgColor = {};
+let cartBtnDot = {};
+if(ticket.status === 'Open') {
+    cartBtnText = {color: '#0B5E06'}
+    cartBtnBgColor = {background: "#B9F8CF"}; 
+    cartBtnDot = {background: "#02A53B"}; 
+}
+else if(ticket.status === 'In Progress') {
+    cartBtnText = { color: "#9C7700"};
+    cartBtnBgColor = {background: '#F8F3B9'}
+    cartBtnDot = {background: '#FEBB0C'}
+    
+}
+
     return (
-        <div>
-            
-        </div>
+       
+        
+            <div className='bg-white p-4 shadow'>
+                <div className='flex justify-between items-center mb-2'>
+                    <h3 className='font-medium text-[18px] leading-5.5 text-[#001931]'>{ticket.title}</h3>
+                    <div style={cartBtnBgColor}   className='flex items-center gap-1.5 font-medium  py-1  rounded-2xl  px-3 '>
+                    <span style={cartBtnDot}  className=' rounded-full w-4 h-4'></span>
+                    <span style={cartBtnText}>{ticket.status}</span>
+                        
+                    </div>
+                </div>
+                <p className='text-[#627382]'>{ticket.description}</p>
+                
+                <div className="flex justify-between items-center mt-4">
+                    <div className='font-medium text-[14px] flex items-center'>
+                    <p className='text-[#627382] mr-4'>{ticket.id}</p>
+                    <p className='text-[#02A53B]'>{ticket.priority}</p>
+                </div>
+                <div className="flex items-center">
+                    <p className='text-[#627382] mr-4'>{ticket.customer}</p>
+                    <img src={clander} alt="" />
+                    <p className='text-[#627382] ml-2'>{ticket.createdAt}</p>
+                </div>
+                </div>
+
+            </div>
+       
     );
 };
 
